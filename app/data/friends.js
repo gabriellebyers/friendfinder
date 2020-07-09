04895ffd6c5e64a447ae49d51fba1280fce4102a
gabriellebyers <gabriellembyers@gmail.com>
2020-07-09 16:03:10 -0500

    added routing

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,5 @@
 
-var friendsArray = [
+var friends = [
 {
     name:"Ahmed",
     photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
@@ -15,8 +15,51 @@ var friendsArray = [
         4,
         1
       ]
-  }
+  },
+
+
+
+
+
+  {
+    name:"Monica",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        3,
+        3,
+        1,
+        1,
+        1,
+        4,
+        5,
+        1,
+        3,
+        3
+      ]
+    },
+
+
+
+
+
+
+    {
+      name:"Daniel",
+      photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+      scores: [
+          4,
+          1,
+          4,
+          1,
+          5,
+          2,
+          5,
+          3,
+          4,
+          1
+        ]
+      }
 ]
 
 
-module.exports = friendsArray;
+module.exports = friends;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
     console.log(__dirname)
